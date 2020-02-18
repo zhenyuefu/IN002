8dd12ef8a77f89aaa9ab5ea3451153534e3036dc
Zhenyue Fu <zhenyue.fu@outlook.com>
2020-02-18 11:20:37 +0000

    fix error

--- TD/ex15.c
+++ TD/ex15.c
@@ -1,17 +1,19 @@
-void permute(int *a, int *b){
+void permute(int *a, int *b) {
   int tmp;
-  tmp = * a;
-  * a = * b;
-  * b = tmp;
+  tmp = *a;
+  *a = *b;
+  *b = tmp;
 }
 
-void permute_circulaire(int *v1, int *v2, int *v3){
-  permute(&v1,&v2);
-  permute(&v2,&v3);
+void permute_circulaire(int *v1, int *v2, int *v3) {
+  permute(v1, v2);
+  permute(v2, v3);
 }
 
-int main(void){
-  int a,b,c;
-  a=10;b=20;c=30;
-  permute_circulaire(&a,&b,&c);
+int main(void) {
+  int a, b, c;
+  a = 10;
+  b = 20;
+  c = 30;
+  permute_circulaire(&a, &b, &c);
 }
